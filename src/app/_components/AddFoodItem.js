bca2000fb6caf0095922b01bf6be4cc099cc22d0
Sonali Agarwal <agarwalsonali922@gmail.com>
2026-07-21 22:21:52 +0530

    added food api

--- src/app/_components/AddFoodItem.js
+++ src/app/_components/AddFoodItem.js
@@ -5,17 +5,20 @@ const AddFoodItem = () => {
     const [price, setPrice] = useState("")
     const [path, setPath] = useState("")
     const [description, setDescription] = useState("")
+    const [error, setError] = useState(false)
 
     const handleAddFoodItem = async () => {
         if(!name || !price || !path || !description){
-            alert("Please fill all fields")
+            setError(true)
             return false;
+        }else{
+            setError(false)
         }   
 
         let resto_id;
         const restoData = JSON.parse(localStorage.getItem("restaurantUser"))
         if(!restoData){
-            alert("Please login first")
+            setError(true)
             return false;
         }else{
             resto_id = restoData._id
@@ -31,12 +34,14 @@ const AddFoodItem = () => {
         let data = await response.json()
         if(data.success){
             alert("Food item added successfully")
+            setError(false)
             setName("")
             setPrice("")
             setPath("")
             setDescription("")
         }else{
-            alert("Failed to add food item")
+            setError(true)
+            alert("Error while adding food item")
         }
     }
 
@@ -46,15 +51,19 @@ const AddFoodItem = () => {
             <div className="input-wrapper">
                 <input type="text" className="input-field" placeholder="Enter food name" value={name} onChange={(e)=>setName(e.target.value)} />
             </div>
+                {error && !name && <span className="input-error">Please enter valid food name</span>}
             <div className="input-wrapper">
                 <input type="text" className="input-field" placeholder="Enter food price" value={price} onChange={(e)=>setPrice(e.target.value)} />
             </div>
+                {error && !price && <span className="input-error">Please enter valid food price</span>}
             <div className="input-wrapper">
                 <input type="text" className="input-field" placeholder="Enter path" value={path} onChange={(e)=>setPath(e.target.value)} />
             </div>
+                {error && !path && <span className="input-error">Please enter valid image path</span>}
             <div className="input-wrapper">
                 <input type="text" className="input-field" placeholder="Enter description" value={description} onChange={(e)=>setDescription(e.target.value)} />
             </div>
+                {error && !description && <span className="input-error">Please enter valid food description</span>}
             <div className="input-wrapper">
                 <button className="button" onClick={handleAddFoodItem}>Add Food Item</button>
             </div>
