2bcbe1ab22db4dbc55fd38ed76b4fd9fa3d0f70b
Sonali Agarwal <agarwalsonali922@gmail.com>
2026-07-22 20:36:25 +0530

    integrate food item list api and component

--- src/app/_components/FoodItemList.js
+++ src/app/_components/FoodItemList.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+
 const FoodItemList = () => {
+
+    const [foodItems, setFoodItems] = useState();
+
+    useEffect(() => {
+        loadFoodItems(); 
+    },[]);
+
+    const loadFoodItems = async () => {
+        const restaurantData = JSON.parse(localStorage.getItem("restaurantUser"));
+        let response = await fetch(`http://localhost:3000/api/restaurant/foods/${restaurantData._id}`);
+        response = await response.json();
+        if(response.success){
+            setFoodItems(response.result);
+        }else{
+            alert("Error while fetching food items");
+        }
+    }
+
   return (
     <div>
         <h1>Food Item List</h1>
@@ -14,14 +34,21 @@ const FoodItemList = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Paneer Tikka</td>
-                    <td>250</td>
-                    <td>Delicious paneer tikka with special spices</td>
-                    <td>Image</td>
-                    <td><button>Delete</button> <button>Edit</button></td>
-                </tr>
+                {
+                    foodItems && foodItems.map((item, key) => (
+                        <tr key={key}>
+                            <td>{key + 1}</td>
+                            <td>{item.name}</td>
+                            <td>{item.price}</td>
+                            <td>{item.description}</td>
+                            <td><img src={item.img_path} alt={item.name} /></td>
+                            <td>
+                                <button>Delete</button>
+                                <button>Edit</button>
+                            </td>
+                        </tr>
+                    ))
+                }
             </tbody>
         </table>
     </div>  
