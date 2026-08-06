0bcc2a739bf3d18b246b1b701ba5a9fd93b93b57
Sonali Agarwal <agarwalsonali922@gmail.com>
2026-08-06 11:59:45 +0530

    Header and Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import CustomerHeader from "./_components/CustomerHeader";
+import RestaurantFooter from "./_components/Footer";
 
 export default function Home() {
   return (
       <main>
+        <CustomerHeader/>
         <h1>Food Delivery App</h1>
+        <RestaurantFooter />
       </main>
   );
 }
