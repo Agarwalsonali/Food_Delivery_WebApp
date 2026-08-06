96f07727d4e74912c2be145c411c4827b5f61e1e
Sonali Agarwal <agarwalsonali922@gmail.com>
2026-08-06 12:45:55 +0530

    location api

---
+++ src/app/api/customer/locations/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+import mongoose from "mongoose";
+import { connectionStr } from "@/app/lib/db";
+import { restaurantSchema } from "@/app/lib/restaurantsModel";
+
+export async function GET(request) {
+    await mongoose.connect(connectionStr);
+    let result = await restaurantSchema.find();
+    result = result.map((item) => item.city.charAt(0).toUpperCase() + item.city.slice(1));
+    result = [...new Set(result.map((item) => item))];
+    return NextResponse.json({ success: true, result });
+}
